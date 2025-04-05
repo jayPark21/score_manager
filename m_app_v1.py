9dc5db4d143020d971005f5d8c5b4a70769687e5
Park coding <97460458+jayPark21@users.noreply.github.com>
2025-04-05 18:35:28 +0900

    Update m_app_v1.py

--- m_app_v1.py
+++ m_app_v1.py
@@ -26,7 +26,7 @@ st.set_page_config(
 
 # st.title("HUGA Golf Score Manager")
 st.markdown("""
-    <h1 style="font-size: 3.2rem; font-weight: 700; color: #4682B4; text-align: center;">
+    <h1 style="font-size: 5.5rem; font-weight: 700; color: #4682B4; text-align: center;">
         🏌️ HUGA Golf Score Manager
     </h1>
 """, unsafe_allow_html=True)
